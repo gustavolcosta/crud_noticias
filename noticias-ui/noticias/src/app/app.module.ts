65ae7e5a89ee39ddd9476e0ce594e524c14d6f9f
Gustavo Costa <gustavolcosta@hotmail.com.br>
2020-08-05 02:08:30 -0300

    Inicio projeto front-end

---
+++ noticias-ui/noticias/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TelaInicialComponent } from "./tela-inicial/tela-inicial.component";
+import { CadastrarNoticiaComponent } from "./cadastrar-noticia/cadastrar-noticia.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatButtonModule,
+  MatDatepickerModule,
+  MAT_DATE_LOCALE,
+  MatNativeDateModule,
+} from "@angular/material";
+import { MatIconModule } from "@angular/material";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+
+@NgModule({
+  declarations: [AppComponent, TelaInicialComponent, CadastrarNoticiaComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: "pt-BR" }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
